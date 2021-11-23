042154edcf97130ec413441f090b20becfa4d037
Nachomontoya <montoyanacho@gmail.com>
2021-11-23 19:15:05 +0100

    starting api

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -2,8 +2,8 @@ import { API } from "../constants/routes";
 
 const axios = require("axios").default;
 
-export function getPopularMovies() {
-  return axios.get(`${API.MAIN}${API.MOVIES}${API.POPULAR}`, {
+export function getPopularMovies(page = 1) {
+  return axios.get(`${API.MAIN}${API.MOVIES}${API.POPULAR}?page=${page}`, {
     headers: { Authorization: `Bearer ${API.API_KEY}` },
   });
 }
