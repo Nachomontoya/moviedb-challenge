d7626f0d8a0a06c5488928c9cdb8924d8bac8726
Nachomontoya <montoyanacho@gmail.com>
2021-11-23 18:51:24 +0100

    constants routes created

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { CardProps } from "../../utils/types";
 
 function Card({ imgUrl, title, votes }: CardProps) {
-  return <div></div>;
+  return (
+    <div>
+      <img src={imgUrl} alt={title} />
+      <h3>{title}</h3>
+      <p>{votes}</p>
+    </div>
+  );
 }
 
 export default Card;
---
+++ src/constants/routes.ts
@@ -0,0 +1,10 @@
+export const { REACT_APP_TMDB_API_ROUTE } = process.env;
+
+export const PUBLIC = {
+  HOME: "/",
+};
+
+export const API = {
+  MAIN: REACT_APP_TMDB_API_ROUTE,
+  ACCOUNT: "/account",
+};
