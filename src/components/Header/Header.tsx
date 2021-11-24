427925596fea3086962edddd5de5b464ce9eab1b
Nachomontoya <montoyanacho@gmail.com>
2021-11-24 13:32:52 +0100

    added link home to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { SiThemoviedatabase } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 function Header(): React.ReactElement {
   return (
     <header className="pb-35">
-      <SiThemoviedatabase className="fnt-logo fnt-white" />
+      <Link to={"/"}>
+        <SiThemoviedatabase className="fnt-logo fnt-white" />
+      </Link>
     </header>
   );
 }
