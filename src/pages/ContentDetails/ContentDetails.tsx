f121c0fe5dc9468e6b714b81ca5c7fcefa64f1a7
Nachomontoya <montoyanacho@gmail.com>
2021-11-25 11:49:26 +0100

    feat: scroll bars hidden

--- src/pages/ContentDetails/ContentDetails.tsx
+++ src/pages/ContentDetails/ContentDetails.tsx
@@ -7,6 +7,7 @@ import Layout from "../../components/Layout";
 import { API } from "../../constants/routes";
 import { DetailsProps, ResultProps } from "../../utils/types";
 import Loader from "react-loader-spinner";
+import Scrollbars from "react-custom-scrollbars";
 
 function ContentDetails(): React.ReactElement {
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -56,76 +57,85 @@ function ContentDetails(): React.ReactElement {
 
   return (
     <Layout>
-      <div className="d-flex flex-column justify-content-between">
-        <div className="row mb-5">
-          {isLoading ? (
-            <Loader type="ThreeDots" color="#63acf0" height={50} width={50} />
-          ) : (
-            <>
-              <div className="col-6 col-md-8">
-                {/* Title  */}
-                <h1 className="fnt-white">{details.title}</h1>
-                {/*  Votes */}
-                <div className="d-flex align-items-center mb-4">
-                  <FaStar className="gold fnt-medium me-1" />
-                  <span className="fnt-white fnt-medium fnt-semibold">
-                    {details.votes}
-                  </span>
-                  <span className="fnt-white m-0 fnt-light pt-1">/10</span>
-                </div>
-                {/* Overview */}
-                <div className="col-md-6 col-xl-8">
-                  <h3 className="fnt-white mb-3">Overview</h3>
-                  <p className="fnt-white fnt-light">{details.overview}</p>
-                </div>
-              </div>
-              <div className="col-6 col-md-4 d-flex align-items-start justify-content-end poster-wrapper-small">
-                {details.image ? (
-                  <img
-                    src={
-                      details.image ? `${API.IMAGES_URL}/${details.image}` : ""
-                    }
-                    alt={details.title}
-                    className="poster-image-small rounded-3"
-                  />
-                ) : (
-                  <div className="no-image" />
-                )}
-              </div>
-            </>
-          )}
-        </div>
-        {/* related */}
-        <div className="col-12">
-          <h3 className="fnt-white mb-3">Related Content</h3>
-          <div className="row">
+      <Scrollbars autoHide>
+        <div className="d-flex flex-column justify-content-between">
+          <div className="row mb-5">
             {isLoading ? (
               <Loader type="ThreeDots" color="#63acf0" height={50} width={50} />
             ) : (
               <>
-                {related ? (
-                  related.map((art, index) => {
-                    while (index < 8) {
-                      return (
-                        <Card
-                          key={art.id}
-                          id={art.id}
-                          imgUrl={`${API.IMAGES_URL}/${art.poster_path}`}
-                          title={art.name || art.title}
-                          votes={art.vote_average?.toFixed(1)}
-                          isMovie={art.title ? true : false}
-                        />
-                      );
-                    }
-                  })
-                ) : (
-                  <p>No related content</p>
-                )}
+                <div className="col-6 col-md-8">
+                  {/* Title  */}
+                  <h1 className="fnt-white">{details.title}</h1>
+                  {/*  Votes */}
+                  <div className="d-flex align-items-center mb-4">
+                    <FaStar className="gold fnt-medium me-1" />
+                    <span className="fnt-white fnt-medium fnt-semibold">
+                      {details.votes}
+                    </span>
+                    <span className="fnt-white m-0 fnt-light pt-1">/10</span>
+                  </div>
+                  {/* Overview */}
+                  <div className="col-md-6 col-xl-8">
+                    <h3 className="fnt-white mb-3">Overview</h3>
+                    <p className="fnt-white fnt-light">{details.overview}</p>
+                  </div>
+                </div>
+                <div className="col-6 col-md-4 d-flex align-items-start justify-content-end poster-wrapper-small">
+                  {details.image ? (
+                    <img
+                      src={
+                        details.image
+                          ? `${API.IMAGES_URL}/${details.image}`
+                          : ""
+                      }
+                      alt={details.title}
+                      className="poster-image-small rounded-3"
+                    />
+                  ) : (
+                    <div className="no-image" />
+                  )}
+                </div>
               </>
             )}
           </div>
+          {/* related */}
+          <div className="col-12">
+            <h3 className="fnt-white mb-3">Related Content</h3>
+            <div className="row">
+              {isLoading ? (
+                <Loader
+                  type="ThreeDots"
+                  color="#63acf0"
+                  height={50}
+                  width={50}
+                />
+              ) : (
+                <>
+                  {related ? (
+                    related.map((art, index) => {
+                      while (index < 8) {
+                        return (
+                          <Card
+                            key={art.id}
+                            id={art.id}
+                            imgUrl={`${API.IMAGES_URL}/${art.poster_path}`}
+                            title={art.name || art.title}
+                            votes={art.vote_average?.toFixed(1)}
+                            isMovie={art.title ? true : false}
+                          />
+                        );
+                      }
+                    })
+                  ) : (
+                    <p>No related content</p>
+                  )}
+                </>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
+      </Scrollbars>
     </Layout>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,28 +3,31 @@ import Button from "../../components/Button";
 import Layout from "../../components/Layout";
 import MovieList from "../../components/MovieList";
 import ShowsList from "../../components/ShowsList";
+import { Scrollbars } from "react-custom-scrollbars";
 
 function Home(): React.ReactElement {
   const [isMovie, setIsMovie] = useState<boolean>(true);
 
   return (
     <Layout>
-      <div className="d-flex justify-content-between py-2 mb-4">
-        <h2 className="fnt-white">Popular {isMovie ? "Movies" : "Shows"}</h2>
-        <div className="d-flex gap-3">
-          <Button
-            txtBtn="Movies"
-            onClick={() => setIsMovie(true)}
-            isActive={isMovie}
-          />
-          <Button
-            txtBtn="Shows"
-            onClick={() => setIsMovie(false)}
-            isActive={!isMovie}
-          />
+      <Scrollbars autoHide>
+        <div className="d-flex justify-content-between py-2 mb-4">
+          <h2 className="fnt-white">Popular {isMovie ? "Movies" : "Shows"}</h2>
+          <div className="d-flex gap-3">
+            <Button
+              txtBtn="Movies"
+              onClick={() => setIsMovie(true)}
+              isActive={isMovie}
+            />
+            <Button
+              txtBtn="Shows"
+              onClick={() => setIsMovie(false)}
+              isActive={!isMovie}
+            />
+          </div>
         </div>
-      </div>
-      {isMovie ? <MovieList /> : <ShowsList />}
+        {isMovie ? <MovieList /> : <ShowsList />}
+      </Scrollbars>
     </Layout>
   );
 }
